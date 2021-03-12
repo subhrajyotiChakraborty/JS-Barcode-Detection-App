4fe2781ded733035bf642893230cfa919342a24d
Subhrajyoti <subha.ionic3@gmail.com>
2021-03-12 14:03:47 +0530

    added sound

--- js/app.js
+++ js/app.js
@@ -77,10 +77,56 @@ function initQuagga(deviceID) {
   );
 }
 
+Quagga.onProcessed(function (result) {
+  var drawingCtx = Quagga.canvas.ctx.overlay,
+    drawingCanvas = Quagga.canvas.dom.overlay;
+
+  if (result) {
+    if (result.boxes) {
+      drawingCtx.clearRect(
+        0,
+        0,
+        parseInt(drawingCanvas.getAttribute("width")),
+        parseInt(drawingCanvas.getAttribute("height"))
+      );
+      result.boxes
+        .filter(function (box) {
+          return box !== result.box;
+        })
+        .forEach(function (box) {
+          Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+            color: "green",
+            lineWidth: 2,
+          });
+        });
+    }
+
+    if (result.box) {
+      Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
+        color: "#00F",
+        lineWidth: 2,
+      });
+    }
+
+    if (result.codeResult && result.codeResult.code) {
+      Quagga.ImageDebug.drawPath(result.line, { x: "x", y: "y" }, drawingCtx, {
+        color: "red",
+        lineWidth: 3,
+      });
+    }
+  }
+});
+
 Quagga.onDetected(function (result) {
   clearInterval(timer);
-  var audio = new Audio("../assets/sound/beep.mp3");
-  audio.play();
+  // var audio = new Audio("../assets/sound/beep.mp3");
+  // audio.play();
+  var sound = new Howl({
+    src: ["../assets/sound/beep.wav"],
+    volume: 1,
+    onend: function () {},
+  });
+  sound.play();
   successHandler(result);
   // setTimeout(function () {
   //   Quagga.offDetected(successHandler);
